cba4828f34b8416b0f88528eeb25eb5bb932f79f
Rodevp <rodriguezrodrigor12@gmail.com>
2021-03-03 10:13:11 -0500

    agregando efecto de menu de cambio de color

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ const menu = document.getElementById('menu');
 const menuItems = document.getElementsByClassName('header__list-item');
 const checkbox = document.getElementById('pitcher');
 const menuHeader = document.querySelector('.header__list');
+const header = document.getElementById('header');
+const hamburguerLineaMenu = document.getElementsByClassName('activator-menu-linea');
 
-console.log(menuHeader);
-
-function mappingItemasClassAddOrRemove(arr) {
+function mappingItemasClassAddOrRemove(arr, nameClassForItem, containClass){
     const arrItems = [...arr];
     arrItems.forEach(item => {
-        if(!item.classList.contains('header__list--item-view-animation')
+        if(!item.classList.contains(containClass)
             && checkbox.checked
         ) {
-            item.classList.add('header__list--item-view-animation');
+            item.classList.add(nameClassForItem);
         }
-        if (item.classList.contains('header__list--item-view-animation')
+        if (item.classList.contains(containClass)
             && !checkbox.checked
         ) {
-            item.classList.remove('header__list--item-view-animation');
+            item.classList.remove(nameClassForItem);
         }
     });
 }
@@ -26,12 +26,42 @@ function mappingItemasClassAddOrRemove(arr) {
 checkbox.addEventListener('click', e => {
     if ( checkbox.checked && !menu.classList.contains(CLASS_MENU) ) {
         menu.classList.add(CLASS_MENU);
-        mappingItemasClassAddOrRemove(menuItems);
+        mappingItemasClassAddOrRemove(
+            menuItems,
+            'header__list--item-view-animation',
+            'header__list--item-view-animation'
+        );
     };
     if ( !checkbox.checked && menu.classList.contains(CLASS_MENU) ) {
         menu.classList.remove(CLASS_MENU);
-        mappingItemasClassAddOrRemove(menuItems);
+        mappingItemasClassAddOrRemove(
+            menuItems,
+            'header__list--item-view-animation',
+            'header__list--item-view-animation'
+        );
     }
     if ( checkbox.checked ) menuHeader.style.display = "block";
     if ( !checkbox.checked ) menuHeader.style.display = "none";
 });
+
+function mappingHmaburguerMenu(items, screenSize) {
+    const itemsHtml = [...items];
+    itemsHtml.forEach( item => {
+        if (screenSize > 0) {
+            item.style.backgroundColor = "white";
+        }
+        if (screenSize === 0) {
+            item.style.backgroundColor = "black";
+        }
+
+    });
+}
+mappingHmaburguerMenu(hamburguerLineaMenu);
+window.onscroll = (e) => {
+    mappingHmaburguerMenu(hamburguerLineaMenu, header.offsetTop);
+    if (header.offsetTop > 0) {
+        header.classList.add('change__header_scroll');
+    } else {
+        header.classList.remove('change__header_scroll');
+    }
+}   
